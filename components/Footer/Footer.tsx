6213d32d3fb7c973f2ac35525f808274f6f70359
heera9331 <heera9331@gmail.com>
2024-03-14 07:03:24 -0700

    footer designed

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,8 +3,8 @@ import React from "react"
 
 const Footer: React.FC = () => {
     return (
-        <footer className="">
-            <b className="text-center">Design & Developed by - Heera Singh</b>
+        <footer className="bg-gray-100 border border-black border-opacity-25 flex justify-between px-4 items-center py-3 text-center font-semibold">
+            Design & Developed by - Heera Singh
         </footer>
     )
 }
