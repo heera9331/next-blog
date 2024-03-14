72cfdccf745530716fe0b8359ded848a267b5046
heera9331 <heera9331@gmail.com>
2024-03-14 11:17:49 -0700

    user route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,15 +1,14 @@
 import { NextResponse, NextRequest } from "next/server";
 import { connectDB } from "@/utils";
-import { Post }from "@/models"; 
+import { User }from "@/models"; 
 
 export const GET = async (request: NextRequest) => {
-    
   try {
     await connectDB();
 
-    const posts = await Post.find({});
+    const users = await User.find({});
 
-    return NextResponse.json(posts);
+    return NextResponse.json(users);
   } catch (err) {
     return new NextResponse("Database Error", { status: 500 });
   }
@@ -18,12 +17,11 @@ export const GET = async (request: NextRequest) => {
 export const POST = async (request:NextRequest) => {
   const body = await request.json();
 
-  const newPost = new Post(body);
+  const newUser = new User(body);
 
   try {
-    await connectDB();
-
-    await newPost.save();
+    await connectDB(); 
+    await newUser.save();
 
     return new NextResponse("Post has been created", { status: 201 });
   } catch (err) {
