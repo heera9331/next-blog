718965a375a865850872beded72207368cb36b7a
heera9331 <heera9331@gmail.com>
2024-03-14 07:04:17 -0700

    padding added to the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <div className="min-h-[90vh]">
+        <div className="min-h-[90vh] md:p-2">
           {children}
         </div>
         <Footer />
