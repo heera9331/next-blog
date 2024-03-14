9653e11c9b10317d3aae11a3fba70b88dfa4b987
heera9331 <heera9331@gmail.com>
2024-03-14 11:10:10 -0700

    db connection

--- util/db.ts
+++ util/db.ts
@@ -1,1 +1,25 @@
 
+import mongoose from "mongoose";
+
+const connection = {};
+const MONGO = process.env.MONGO;
+
+const connectDB = async () => {
+  try {
+    if (connection.isConnected) {
+      console.log("Using existing connection");
+      return;
+    }
+    console.log(MONGO);
+    const db = await mongoose.connect(MONGO);
+    console.log('connected to db');
+    // const db = await mongoose.connect(process.env.MONGO);
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// connectDB("mongodb://127.0.0.1:27017/COLLEGE_COUNSELLING");
+
+export default connectDB;
