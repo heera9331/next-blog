b99296b81a4df7b83ea46794c1104cb4a57ed58a
heera9331 <heera9331@gmail.com>
2024-03-14 06:34:33 -0700

    header, footer added

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+
+import React from "react"
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="">
+            <b className="text-center">Design & Developed by - Heera Singh</b>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header: React.FC = () => {
+    return (
+        <header className="">
+            <h1 className="font-semibold text-2xl">Logo</h1>
+        </header>
+    )
+}
+
+export default Header;
