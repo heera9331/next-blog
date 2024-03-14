6a8ed9b723d2fc4190cbabb367e0b95f1d47eed8
heera9331 <heera9331@gmail.com>
2024-03-14 06:40:50 -0700

    register and sitemap created

---
+++ app/sitemap/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Page() {
+
+    return (
+        <div>
+            <h1 className="font-semibold text-2xl">SiteMap</h1>
+
+            <div className="flex">
+                <a href="/" className="underline text-blue-800">Home</a>
+            </div>
+        </div>
+    )
+}
