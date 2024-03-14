f6555f5c0c104d8db6b052cbb237ce689b88a493
heera9331 <heera9331@gmail.com>
2024-03-14 07:06:18 -0700

    sitemap created

--- app/sitemap/page.tsx
+++ app/sitemap/page.tsx
@@ -6,8 +6,11 @@ export default function Page() {
         <div>
             <h1 className="font-semibold text-2xl">SiteMap</h1>
 
-            <div className="flex">
+            <div className="flex flex-col">
                 <a href="/" className="underline text-blue-800">Home</a>
+                <a href="/login" className="underline text-blue-800">Login</a>
+                <a href="/register" className="underline text-blue-800">Register</a>
+                <a href="/sitemap" className="underline text-blue-800">Sitemap</a>
             </div>
         </div>
     )
