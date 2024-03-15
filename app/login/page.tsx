117ed8f6f6548bd3b1fe07241cd8448a251c4d0d
heera9331 <heera9331@gmail.com>
2024-03-14 19:58:01 -0700

    login done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,36 +4,33 @@ import "../globals.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Loading, Input, Button } from "@/components";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { getProviders, signIn, useSession } from "next-auth/react";
 
 const Page = () => {
     const [user, setUser] = useState({
         username: "",
         password: "",
     });
+    const session = useSession();
+    const params = useSearchParams();
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
 
     const [loading, setLoading] = useState<boolean>(false);
     const router = useRouter();
+
     let timeout: any = null;
 
     const handleLogin = async () => {
         try {
-            console.log(user);
+            console.log('user to be login', user);
             setLoading(true);
-            timeout = setTimeout(() => { }, 2000);
-            let res = await axios.post(`/api/auth`, user);
-            console.log(res);
-            let data = res.data;
+            signIn("credentials", {
+                username: user.username,
+                password: user.password,
+            });
             setLoading(false);
-
-            if (data?.error) {
-                alert(data.error);
-            } else {
-                console.log(data);
-                // token, isAdmin, userId 
-                setLoading(false);
-                router.push("/home");
-            }
         } catch (error) {
             setLoading(false);
             alert("Server connection timeout");
@@ -42,64 +39,74 @@ const Page = () => {
         setLoading(false);
     };
 
+
     useEffect(() => {
+        setError(params.get("error") || "");
+        setSuccess(params.get("success") || "");
+
         return () => {
             clearInterval(timeout);
         };
-    }, [timeout]);
+    }, [params, timeout]);
+
+    if (session.status === "loading") {
+        return <p>Loading...</p>;
+    }
+
+    if (session.status === "authenticated") {
+        router?.push("/profile");
+    }
 
     return (
 
         <div className="flex flex-col m-1 h-[78vh]">
             <div className="p-4 m-auto shadow-sm shadow-slate-600 sm:w-[450px]">
-                <>
-                    <div className="px-2">
-                        <h1 className="text-2xl font-semibold">Login</h1>
-                        <form
-                            className="text-black p-4"
-                            action="#"
-                            method="post"
-                            onSubmit={(e) => {
-                                e.preventDefault();
+                <div className="px-2">
+                    <h1 className="text-2xl font-semibold">Login</h1>
+                    <form
+                        className="text-black p-4"
+                        action="#"
+                        method="post"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            handleLogin();
+                        }}
+                    >
+                        <Input
+                            inputColor={"text-black"}
+                            label={"Username"}
+                            htmlFor={"username"}
+                            value={user.username}
+                            placeholder={"Username"}
+                            className={
+                                "bg-gray-100 p-1 text-black rounded-sm focus:outline-none"
+                            }
+                            type={"text"}
+                            onChange={(e: any) => {
+                                setUser({ ...user, username: e.target.value });
                             }}
-                        >
-                            <Input
-                                inputColor={"text-black"}
-                                label={"Username"}
-                                htmlFor={"username"}
-                                value={user.username}
-                                placeholder={"Username"}
-                                className={
-                                    "bg-gray-100 p-1 text-black rounded-sm focus:outline-none"
-                                }
-                                type={"text"}
-                                onChange={(e: any) => {
-                                    setUser({ ...user, username: e.target.value });
-                                }}
-                            />
-                            <Input
-                                inputColor={"text-black"}
-                                label={"Password"}
-                                htmlFor={"password"}
-                                value={user.password}
-                                placeholder={"Password"}
-                                className={"bg-gray-100 p-1 rounded-sm focus:outline-none"}
-                                type={"password"}
-                                onChange={(e) => {
-                                    setUser({ ...user, password: e.target.value });
-                                }}
-                            />
+                        />
+                        <Input
+                            inputColor={"text-black"}
+                            label={"Password"}
+                            htmlFor={"password"}
+                            value={user.password}
+                            placeholder={"Password"}
+                            className={"bg-gray-100 p-1 rounded-sm focus:outline-none"}
+                            type={"password"}
+                            onChange={(e) => {
+                                setUser({ ...user, password: e.target.value });
+                            }}
+                        />
 
-                            <div className="flex justify-center">
-                                <Button
-                                    text="Login"
-                                    onClick={handleLogin}
-                                    className="flex items-center justify-center text-black bg-gray-100  m-2 rounded-sm my-4 cursor-pointer"
-                                />
-                            </div>
-                        </form>
-                    </div>
-                </>
+                        <div className="flex justify-center pt-2 flex-col items-center">
+                            <button className="bg-white border border-black border-opacity-25 rounded-sm px-2 py-1 font-semibold hover:bg-gray-100">
+                                Login
+                            </button>
+                            {error && error}
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
 
