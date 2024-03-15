2d16f325e1fec1fd08152056e950f21cfd0d5d58
heera9331 <heera9331@gmail.com>
2024-03-14 20:12:01 -0700

    login redirect

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,10 +1,18 @@
 "use client"
 import { useSession } from "next-auth/react"
-
+import { useRouter } from "next/navigation";
 const Page = () => {
     const session = useSession();
+    const router = useRouter();
 
     console.log(session);
+    if (session.status === "loading") {
+        return <p>Loading...</p>;
+    }
+
+    if (session.status === "unauthenticated") {
+        router?.push("/login");
+    }
 
     return (
         <div>
